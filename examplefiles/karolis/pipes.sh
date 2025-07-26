c9154282b6a5dec2889253161a4b0916657f809d
Karolis Ramanauskas <karolisr@gmail.com>
2025-07-26 11:36:08 -0700

    Committing what I previously forgot to.

--- examplefiles/karolis/pipes.sh
+++ examplefiles/karolis/pipes.sh
@@ -3,3 +3,7 @@
 # shellcheck disable=SC2012
 # https://www.shellcheck.net/wiki/SC2012
 ls -1 | wc -l
+
+cat -p Firstgenes.fa | grep ">"
+cat -p Firstgenes.fa | grep -P "(?<=\>)(.*)"
+cat -p Firstgenes.fa | grep -P -o "(?<=\>)(.*)"
