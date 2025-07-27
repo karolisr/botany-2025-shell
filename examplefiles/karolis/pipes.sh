49478a9d2127ba5d1f8f949e6d798efb15508d74
Karolis Ramanauskas <karolisr@gmail.com>
2025-07-27 06:36:29 -0700

    Ugly final edits.

--- examplefiles/karolis/pipes.sh
+++ examplefiles/karolis/pipes.sh
@@ -4,6 +4,8 @@
 # https://www.shellcheck.net/wiki/SC2012
 ls -1 | wc -l
 
+cd examplefiles/intro_to_CLI
+
 cat -p Firstgenes.fa | grep ">"
 cat -p Firstgenes.fa | grep -P "(?<=\>)(.*)"
 cat -p Firstgenes.fa | grep -P -o "(?<=\>)(.*)"
